772b838afd2120f5906e7faf822defaf06c2f96d
Amnhed <aalhe0.0@gmail.com>
2020-09-07 23:51:38 -0500

    Mover personaje con teclas
    En proyecto=> Ajustes de proyecto => mapa de entrada Creo una acion tecla_a en el boton de mas eligo tecla y presiono la tecla que realizara la accion

--- jugador1.gd
+++ jugador1.gd
@@ -7,19 +7,26 @@ extends Position2D
 
 var Velocidad = Vector2()#velocidad en x,y
 export (float) var GRAVEDAD = 100
+export (float) var VELOCIDAD_MOVIMIENTO = 25
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 func _physics_process(delta):
 	#calcular la velocidad en 'y'
-	Velocidad.y += GRAVEDAD * delta #Gravedad * tiempo
+	Velocidad.y += GRAVEDAD * delta #Gravedad * tiempo Caida
+	
+	if(Input.is_action_pressed("teacla_a")):
+		Velocidad.x = -VELOCIDAD_MOVIMIENTO
+	elif(Input.is_action_pressed("tecla_d")):
+		Velocidad.x = VELOCIDAD_MOVIMIENTO
+	else:
+		Velocidad.x = 0
+		
 	
 	var movimiento = Velocidad * delta # velocidad por tiempo
-	
 	get_node("cuerpo_j1").move_and_slide(movimiento)#nodem que quiero mover
 
 
