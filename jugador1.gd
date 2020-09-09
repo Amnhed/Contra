cbe3ef0fbf6a70a09319b998243dfc7f730a92be
Amnhed <aalhe0.0@gmail.com>
2020-09-08 20:02:09 -0500

    SI esta en el suelo salta
    SI esta presionada la tecla z paramos la animacionde correr

--- jugador1.gd
+++ jugador1.gd
@@ -9,6 +9,7 @@ var Velocidad = Vector2()#velocidad en x,y
 export (float) var GRAVEDAD = 100
 export (float) var VELOCIDAD_MOVIMIENTO = 25
 export (float) var VELOCIDAD_SALTO = 25
+var puede_saltar = false
 
 
 # Called when the node enters the scene tree for the first time.
@@ -31,11 +32,13 @@ func _physics_process(delta):
 		Velocidad.x = VELOCIDAD_MOVIMIENTO
 	else:
 		Velocidad.x = 0
-		get_node("animacion_j1").play("j_idle")
+		if(puede_saltar): 
+			get_node("animacion_j1").play("j_idle") #si puede saltar esta en el suelo entoncle es el personaje en reposo
 		
-	if(Input.is_action_pressed("tecla_z")):
+	if(Input.is_action_pressed("tecla_z") && puede_saltar):
 		Velocidad.y -= VELOCIDAD_SALTO
 		get_node("animacion_j1").play("j1_salto")
+		puede_saltar = false
 		
 	
 	var movimiento = Velocidad * delta # velocidad por tiempo
@@ -47,9 +50,9 @@ func _physics_process(delta):
 		var obj_colisionado = get_node("cuerpo_j1").get_slide_collision(get_node("cuerpo_j1").get_slide_count()-1).collider
 		#obtengo el numero total de colisionadores y el utimo colisionador con '.colider' obtenemos el ojeto de colision
 		if(obj_colisionado.is_in_group("suelo")):
-			print("Hola")
-	else:
-		print("byee")
+			if(puede_saltar == false):
+				puede_saltar = true
+				get_node("animacion_j1").stop()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
